ce0d7d68844d17cab68ad602e0baba849eaab291
Ana <109041866+anasaftoiu@users.noreply.github.com>
2023-08-29 21:07:45 +0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,12 +1,1 @@
-const mongoose = require("mongoose");
 
-//Scos password din mongodb atlas 
-const url = "mongodb+srv://saftoiuana23:<password>@anaproject.itrzbwh.mongodb.net/smsdb?retryWrites=true&w=majority"
-
-mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    
-  }).then(() => {
-    console.log('Db este conectata')
-  }).catch((err) => console.log('Conectare reusita : =', err));
